e8f65aba0e661c6a871c239fed7debee2567c110
unknown <1009154918@qq.com>
2019-05-28 21:10:53 +0800

    update check

--- js/genus.js
+++ js/genus.js
@@ -1,5 +1,9 @@
 $(document).ready(function () {
     var genus;
+    //打开弹出框，去掉验证信息显示
+    $('#exampleModal-info').on('shown.bs.modal',function () {
+
+    });
 
     $.ajax({
         crossDomain: true,
@@ -19,8 +23,27 @@ $(document).ready(function () {
         idField: 'id',
         data: genus,
         columns: [{
-            checkbox: true
-        }, {
+            checkbox: true,
+        },
+            {
+                field:'',//数据列
+                title:'操作',//数据列名称
+                width:'90px',
+                align:'center',//水平居中
+                valign:'middle',//垂直居中
+                formatter:function(value,row,index){//格式化，自定义内容
+                    var _html='';
+
+
+                    _html += '<button  onclick="check('+row.id+')"class="btn btn-primary btn-xs" data-toggle="tooltip" data-placement="bottom" title="查看"><i class="fa fa-search"></i></button>'
+
+                    return _html;
+                },
+                cellStyle:function(value,row,index,field){
+                    return{css:{'min-width':'100px'}};
+                }
+            },
+   {
             field: 'genusNameCh',
             align: 'center',
             title: '中文名',
@@ -40,15 +63,6 @@ $(document).ready(function () {
             align: 'center',
             title: '别名',
             sortable: 'true'
-        }, {
-            field: 'genusDesc',
-            align: 'center',
-            title: '描述',
-            sortable: 'true',
-            valign:'middle',//垂直居中
-            cellStyle:function(value,row,index,field){
-                return{css:{'min-width':'80px','max-width':'150px','word-break': 'break-all'}};
-            }
         }
 
 
@@ -60,6 +74,8 @@ $(document).ready(function () {
             var genusNameEn = $("#genusNameEn").val();
         var genusNameLd = $("#genusNameLd").val();
         var genusNameOth = $("#genusNameOth").val();
+        var genusDesc = $('#demo-summernote').summernote('code');
+
             if (genusNameCh.length == 0) {
                 alert("中文名不能为空")
             } else if (genusNameEn.length == 0) {
@@ -69,9 +85,12 @@ $(document).ready(function () {
                     "genusNameCh": $("#genusNameCh").val(),
                     "genusNameEn": $("#genusNameEn").val(),
                     "genusNameLd": $("#genusNameLd").val(),
-                    "genusNameOth": $("#genusNameOth").val()
-                    var content = $('#demo-summernote').summernote('code');
+                    "genusNameOth": $("#genusNameOth").val(),
+                    "genusDesc": $("#demo-summernote").summernote('code'),
+
+
                 };
+
 
                 $.ajax({
                     type: 'post',
@@ -108,49 +127,62 @@ $(document).ready(function () {
 
 
     })
-    //查看详情
-    function check(id) {
-        init_info();
-        $.ajax({
-            url:baseUrl+'/genus/findById/',		//请求路径
-            type:'GET',			                    //请求方式
-            dataType:"JSON",		                //返回数据类型
-            contentType: 'application/json',        //数据类型
-            success:function(res){	                //请求成功回调函数
-                if(res.code===200){
-                    $('#genusNameCh').html(res.data.genusNameCh).attr('data-original-title',res.data.genusNameCh);
-                    $('#genusNameEn').html(res.data.genusNameEn).attr('data-original-title',res.data.genusNameEn);
-                    $('#genusNameLd').html(res.data.genusNameLd).attr('data-original-title',res.data.genusNameLd);
-                    $('#genusNameOth').html(res.data.genusNameOth).attr('data-original-title',res.data.genusNameOth);
-                    //$('#specDesc-info').html(res.data.spec.specDesc).attr('data-original-title',res.data.specDesc);
-                    //$('#demo-summernote').summernote('code',res.data.specDesc).attr('data-original-title',res.data.specDesc);
-                    //$('#demo-summernote-info').summernote('code',res.data.genusDesc);
-                    $('#genusDesc').html(res.data);
-                    //$('#genus-info').html(res.data.genus.genusNameCh).attr('data-original-title',res.data.genusNameCh);
-                    $('#exampleModal-info').modal('show');
-                }else if(res.code === 404){
-                    window.location.href='../../page-404.html';
-                }
-                else if(res.code === 505){
-                    window.location.href='../../page-500.html';
-                }
-                else{
-                    $.niftyNoty({
-                        type: 'danger',
-                        icon: 'pli-cross icon-2x',
-                        message: res.msg,
-                        container: 'floating',
-                        timer: 2000
-                    });
-                }
-            },
-            error:function(XMLHttpRequest, textStatus, errorThrown) {//请求失败回调函数
-
-            }
-        });
-    }
-
-
-
 
 });
+//查看详情
+function check(id) {
+    init_info();
+    $.ajax({
+        url:ipValue + '/genus/findById',		//请求路径
+        type:'GET',			                    //请求方式
+        dataType:"JSON",		                //返回数据类型
+        contentType: 'application/json',        //数据类型
+        data: {"id":id},
+        success:function(res){	                //请求成功回调函数
+            console.log(res)
+            if(res.code===200){
+                $('#genusNameCh-info').html(res.data.genusNameCh).attr('data-original-title',res.data.genusNameCh);
+                $('#genusNameEn-info').html(res.data.genusNameEn).attr('data-original-title',res.data.genusNameEn);
+                $('#genusNameLd-info').html(res.data.genusNameLd).attr('data-original-title',res.data.genusNameLd);
+                $('#genusNameOth-info').html(res.data.genusNameOth).attr('data-original-title',res.data.genusNameOth);
+                //$('#specDesc-info').html(res.data.spec.specDesc).attr('data-original-title',res.data.specDesc);
+                //$('#demo-summernote').summernote('code',res.data.specDesc).attr('data-original-title',res.data.specDesc);
+                //$('#demo-summernote-info').summernote('code',res.data.genusDesc);
+                $('#genusDesc-info').html(res.data.genusDesc);
+                //$('#genus-info').html(res.data.genus.genusNameCh).attr('data-original-title',res.data.genusNameCh);
+                $('#exampleModal-info').modal('show');
+            }else if(res.code === 404){
+                window.location.href='../../page-404.html';
+            }
+            else if(res.code === 505){
+                window.location.href='../../page-500.html';
+            }
+            else{
+                $.niftyNoty({
+                    type: 'danger',
+                    icon: 'pli-cross icon-2x',
+                    message: res.msg,
+                    container: 'floating',
+                    timer: 2000
+                });
+            }
+        },
+        error:function(XMLHttpRequest, textStatus, errorThrown) {//请求失败回调函数
+
+        }
+    });
+}
+function init_info(){
+
+    $('#genusId').val("").attr('data-original-title',"");
+    $('#genusNameCh-info').val("").attr('data-original-title',"");
+    $('#genusNameEn-info').val("").attr('data-original-title',"");
+    $('#genusNameLd-info').val("").attr('data-original-title',"");
+    $('#genusNameOth-info').val("").attr('data-original-title',"");
+    $('#genusDesc-info').val("").attr('data-original-title',"");
+
+}
+
+
+
+
