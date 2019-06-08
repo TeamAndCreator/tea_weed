2bf9471d6eb0ab97b934aecd9920ce4a0bd72108
743551828 <743551828@qq.com>
2019-06-08 18:25:34 +0800

    update photo

--- js/ipaddress.js
+++ js/ipaddress.js
@@ -1,6 +1,6 @@
 
-/*var ipValue="http://47.104.26.79:7808/";
-var vr_path="http://47.104.26.79:7808/file/";*/
+var ipValue="http://47.104.26.79:7808/";
+var vr_path="http://47.104.26.79:7808/file/";
 
-var ipValue="http://localhost:8080/";
-var vr_path="http://localhost:8080/file/";
+// var ipValue="http://localhost:8080/";
+// var vr_path="http://localhost:8080/file/";
